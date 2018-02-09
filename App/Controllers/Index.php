93e117a0c26fc99d6d4028cf94a63fc85f884fb0
Guilherme Ferreira <gff105@gmail.com>
2018-02-09 15:57:17 -0200

    Passando variaveis para uma pagina

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -6,17 +6,38 @@ namespace App\Controllers;
 //classe controller
 class Index{
 
+	private $view;
+
+	//construtor
+	public function __construct(){
+
+		//criando um objeto vazio
+		$this->view = new \stdClass;
+
+	}
+
+	// action
+	public function index(){
+
+		$this->view->x = 1;
+
+		$nomes = array();
+		$nomes[] = "Guilherme";
+		$nomes[] = "Ferreira";
+
+		//adicionando o atributo $nomes ao objeto view
+		$this->view->nomes = $nomes;
+
+		// a variavel será passada para a pagina abaixo
+		
+		include '../App/views/Index/Index.php';;
+	}
+
+
 	// action
 	public function empresa(){
 		include '../App/views/Index/Empresa.php';
 	}
-
-
-	// action
-	public function index(){
-		include '../App/views/Index/Index.php';;
-	}
-
 }
 
 ?>
