883a919418ac84e95863526f2a17d532bf67f5dd
Guilherme Ferreira <gff105@gmail.com>
2018-02-22 16:50:48 -0300

    Mudancas no model

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -3,26 +3,25 @@
 namespace App\Controllers;
 
 use SON\Controller\Action;
+use \App\Models\Artigo;
 
 //classe controller
 class Index extends Action{
 
-	
-
 	// action
 	public function index(){
 
-		$nomes = array();
-		$nomes[] = "Guilherme";
-		$nomes[] = "Ferreira";
+		$artigo = new Artigo(\App\Init::getDB())
+		//chamando o model
+
+		$artigos = $artigos->fetch_all();
 		
-		$this->view->nomes = $nomes;
+		$this->view->artigos = $artigos;
 		//atribuindo dados para a view: adicionando o atributo $nomes ao objeto view
 
 		$this->render('index');
 		//renderizando: a variavel é passada para a pagina		
 	}
-
 
 	// action
 	public function empresa(){
